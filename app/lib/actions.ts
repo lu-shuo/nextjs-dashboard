3bfeace6fb7ca1bd12b180457d95481d9e6c30a2
lushuo <lushuo@kedacom.com>
2025-03-29 15:11:49 +0800

    feat: chapter12 server action

---
+++ app/lib/actions.ts
@@ -0,0 +1,66 @@
+'use server'
+// By adding the 'use server', you mark all the exported functions within the file as Server Actions.
+
+import { z } from 'zod'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import postgres from 'postgres'
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+})
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true })
+export async function createInvoice(formData: FormData) {
+  // If you're working with forms that have many fields, you may want to consider using the entries() method with JavaScript's Object.fromEntries().
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  })
+  // It's usually good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors and ensure greater accuracy.
+  const amountInCents = amount * 100
+  const date = new Date().toISOString().split('T')[0]
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `
+
+  // Once the database has been updated, the /dashboard/invoices path will be revalidated, and fresh data will be fetched from the server.
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true })
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  })
+  const amountInCents = amount * 100
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId},
+        amount = ${amountInCents},
+        status = ${status}
+    WHERE id = ${id}
+  `
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`
+    DELETE FROM invoices
+    WHERE id = ${id}
+  `
+  revalidatePath('/dashboard/invoices')
+}
